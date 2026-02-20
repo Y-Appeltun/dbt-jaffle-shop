d30ebb847962416264fc73210f5eb34ff6b13f84
Yang Appeltun <yangtun21@gmail.com>
2026-02-20 21:20:27 +0000

    Added documentation to the staging table

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -2,19 +2,21 @@ with
 
 source as (
 
-    select * from {{ source('jaffle_shop', 'customers') }}
+    select *
+    from {{ source('jaffle_shop', 'customers') }}
 
 ),
 
 renamed as (
 
     select
-        id as customer_id,
-        first_name,
-        last_name
+        id as customer_id
+        , first_name
+        , last_name
 
     from source
 
 )
 
-select * from renamed
+select *
+from renamed
